24754fed5a22ee736730150b6318932a66d658fb
sidjaix <sidjaix@live.com>
2026-04-10 19:05:36 +0530

    refactor: Remove Logging project references from Dockerfiles and update User-AzureFunctions project to use Api-Common

--- UserAccess/User-AzureFunctions/Program.cs
+++ UserAccess/User-AzureFunctions/Program.cs
@@ -3,28 +3,28 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using UserAccess.AzureFunctions;
 using Microsoft.Extensions.Configuration;
-using Logging.Extensions;
+using ApiCommon.Extensions;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        var host = new HostBuilder()
-        .AddLogging("User-AzureFunctions")
-        .ConfigureFunctionsWebApplication()
-        .ConfigureAppConfiguration((context, config) =>
-        {
-            config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                  .AddEnvironmentVariables();
-        })
-        .ConfigureServices((context, services) =>
-        {
-            services.AddApplicationInsightsTelemetryWorkerService();
-            services.ConfigureFunctionsApplicationInsights();
-            services.RegisterService(context.Configuration);
-        })
-        .Build();
+	private static void Main(string[] args)
+	{
+		var host = new HostBuilder()
+		.AddStandardSerilog("User-AzureFunctions")
+		.ConfigureFunctionsWebApplication()
+		.ConfigureAppConfiguration((context, config) =>
+		{
+			config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+				  .AddEnvironmentVariables();
+		})
+		.ConfigureServices((context, services) =>
+		{
+			services.AddApplicationInsightsTelemetryWorkerService();
+			services.ConfigureFunctionsApplicationInsights();
+			services.RegisterService(context.Configuration);
+		})
+		.Build();
 
-        host.Run();
-    }
+		host.Run();
+	}
 }
