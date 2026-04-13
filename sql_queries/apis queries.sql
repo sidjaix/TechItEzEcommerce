df3c7d5248b2904d9176c608fdbf7791528b9390
sidjaix <sidjaix@live.com>
2026-04-13 14:37:15 +0530

    Refactor SQL queries: remove deprecated apis queries file and create new apis queries.sql with updated queries for ProductApiDb and CartApiDb

---
+++ sql_queries/apis queries.sql
@@ -0,0 +1,17 @@
+select *
+from ProductApiDb.cat.CatalogItems
+WHERE id='943d433e-f36b-1410-8adb-00ab2e36627d'
+
+select *
+from ProductApiDb.cat.ProductVariants
+where CatalogItemId='943d433e-f36b-1410-8adb-00ab2e36627d'
+select *
+from ProductApiDb.cat.ProductVariants
+where id='d640433e-f36b-1410-8adb-00ab2e36627d'
+
+select *
+from CartApiDb.bsk.Carts
+select *
+from CartApiDb.bsk.CartItems
+
+
